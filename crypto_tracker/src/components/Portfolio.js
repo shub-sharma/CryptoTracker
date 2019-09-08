f0401aa995b4c62598ee02da31e0c6abc4a88e5c
Shubham Sharma <shub.sharma@mail.utoronto.ca>
2019-09-08 18:51:11 -0400

    Pass state from Market to Portfolio

--- crypto_tracker/src/components/Portfolio.js
+++ crypto_tracker/src/components/Portfolio.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const Portfolio = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Track Your Crypto Assets</h4>
-        </div>
-    )
+export default class Portfolio extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            portfolio: null,
+        }
+        
+    }
+
+    componentDidMount() {
+        const portfolioData = this.props.portfolio;
+        this.setState({portfolio: portfolioData});
+    }
+
+
+
+
+
+
+    render() {
+        return (
+            <div className="container">
+                <h4 className="center">Track Your Crypto Assets</h4>
+            </div>
+        )
+    }
 }
 
-export default Portfolio;
